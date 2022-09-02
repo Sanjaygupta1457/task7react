108d521575f41f32cd06c2b571fefbd1db1abb22
Sanjaygupta1457 <sanjaygupta1457@gmail.com>
2022-09-02 23:22:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './Mycomponents/ExpenseItem';
+
+
+
+function App(){
+  return (
+    <ExpenseItem></ExpenseItem>
+  );
+}
+
+export default App;
---
+++ src/Mycomponents/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem(){
+    return <><h2>Expense Item!</h2>
+      <h5>Food Rs 10</h5>
+     <h5>Petrol Rs 100</h5>
+     <h5>Movies Rs 200</h5></>
+  
+}
+
+export default ExpenseItem;
